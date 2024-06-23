5fd7da3c3a077cd7df76c741da40e51a905329d5
santizafer <santizafer@gmail.com>
2024-06-22 20:58:10 -0500

    Pregunta 2

--- src/app/dashboard/preguntas/pregunta-2/pregunta-2.component.ts
+++ src/app/dashboard/preguntas/pregunta-2/pregunta-2.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta-2',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pregunta-2.component.html',
   styleUrl: './pregunta-2.component.css'
 })
 export class Pregunta2Component {
+
+  utilidades = ""
+
+  calcular(salario: HTMLInputElement, anios: HTMLInputElement):void{
+
+    var salario_ = Number(salario.value)
+    var anios_ = Number(anios.value)
+    var salarioTotal = 0
+
+
+    if(anios_ < 1) {
+      salarioTotal = salario_ * 0.05
+    } else if (anios_ >= 1 && anios_ < 2 ) {
+      salarioTotal = salario_ * 0.07
+    } else if (anios_ >= 2 && anios_ < 5 ) {
+      salarioTotal = salario_ * 0.07
+    } else if (anios_ >= 5 && anios_ < 10 ) {
+      salarioTotal = salario_ * 0.15
+    } else {
+      salarioTotal = salario_ * 0.20
+    }
+
+    this.utilidades = `utilidades es igual a ${salarioTotal}`
+
+
+  }
+
 
 }
--- src/app/dashboard/preguntas/pregunta-3/pregunta-3.component.ts
+++ src/app/dashboard/preguntas/pregunta-3/pregunta-3.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta-3',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pregunta-3.component.html',
   styleUrl: './pregunta-3.component.css'
 })
 export class Pregunta3Component {
+
+  costototal = ""
+
+  calcular(materias: HTMLInputElement):void{
+
+    var n_materias = Number(materias.value)
+    var precio = 520
+    var costo_total = 0
+
+
+    if(n_materias <= 5) {
+      costo_total = n_materias * precio
+    } else {
+      costo_total = (n_materias * precio) - ((n_materias * precio) * 0.1)
+    }
+    this.costototal = `costo total de las materias es : ${costo_total}`
+
+
+  }
 
 }
